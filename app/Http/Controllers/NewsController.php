374cc87a1231290e058a18eb696fd6fe07ef7a83
aunbvodoi111 <phamquycntta@gmail.com>
2020-02-14 23:39:18 +0700

    update

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -158,7 +158,7 @@ $detail = $dom->saveHTML( $dom->documentElement );
 				$file=$res->file('image');
 				$name= $file->getClientOriginalName();
 				$image = str_random()."_".$name;
-				$file->move("img",$image);
+				$file->move("/img",$image);
 				$news->image = $image;
 			}
     	$news->save();
